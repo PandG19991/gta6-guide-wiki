9ef29cee025fc6ef6b59e0bd23ea4d9b7ff0f296
PandG19991 <92711909+PandG19991@users.noreply.github.com>
2026-07-10 03:57:04 +0800

    chore: remove duplicate site url config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://leonida-ledger.pandg1991.workers.dev",
   output: "static",
   build: {
     format: "directory"
